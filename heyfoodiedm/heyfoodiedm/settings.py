b0004cfa7cee37c80f172e58cd7ddab50cab7dce
GIFS <gifftty.giff@gmail.com>
2020-11-23 23:42:53 +0700

    edit setting

--- heyfoodiedm/heyfoodiedm/settings.py
+++ heyfoodiedm/heyfoodiedm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qv(j*@r*yem&66pfrn8gs$!srlm^p9!5@-5xaby@h0fe*8&0wf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-env.eba-jvpx3kxj.ap-southeast-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-test-env.eba-mtzcxtpg.ap-southeast-1.elasticbeanstalk.com']
 
 
 # Application definition
